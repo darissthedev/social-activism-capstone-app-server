7a358ca544255c093707301a20842df5611a986b
Dariss Harris <darissharris@gmail.com>
2020-12-15 17:33:13 -0800

    cleaning up

--- src/middleware/jwt-auth.js
+++ src/middleware/jwt-auth.js
@@ -1,9 +1,9 @@
 const AuthService = require('../auth/auth-service');
 
 function requireAuth(req, res, next) {
-  // console.log(bearerToken);
+
   const authToken = req.get('Authorization') || '';
-  // console.log(authToken);
+
 
   let bearerToken;
   if (!authToken.toLowerCase().startsWith('bearer ')) {
@@ -11,7 +11,7 @@ function requireAuth(req, res, next) {
   } else {
     bearerToken = authToken.slice(7, authToken.length);
   }
-  // console.log(bearerToken);
+  
   try {
     const payload = AuthService.verifyJwt(bearerToken);
 
@@ -21,18 +21,18 @@ function requireAuth(req, res, next) {
     )
       .then(user => {
         if (!user){
-          console.log('no user in db');
+          
           return res.status(401).json({ error: 'Unauthorized request' });
         }
         req.user = user;
         next();
       })
       .catch(err => {
-        console.error(err);
+       
         next(err);
       });
   } catch(error) {
-    console.log(error);
+    
     res.status(401).json({ error: 'Unauthorized request' });
   }
 }
